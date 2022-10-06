b9cb4c5dc6ccabd4bdc57681f86060da38542d5f
정현정 <33312138+net7281@users.noreply.github.com>
2022-10-06 09:39:02 +0900

    휠이벤트 빼고 드래그이벤트

--- js/script.js
+++ js/script.js
@@ -16,26 +16,29 @@ $(function(){
     //roll banner
     var widthSize =1510;
     var $rollBanner = $('.roll_banner');
+    var $rollBannerIn = $('.roll_banner_in');
 
     var $scrollBar = $('.scroll_bar');
     var $scrollBarIn = $('.scroll_bar_in');
     var scrollBarMax = 160;
     var scrollMax = 510;
 
-    $rollBanner.mousewheel(function(event, delta) {
-        this.scrollLeft -= (delta * 30);
-        event.preventDefault();
-
-        var scrollW= this.scrollLeft*(scrollBarMax/scrollMax);
-        $scrollBarIn.css("left",scrollW+"px");
-    });
+    // $rollBanner.mousewheel(function(event, delta) {
+    //     // this.scrollLeft -= (delta * 30);
+    //     var scrollLeft = this.scrollLeft
+    //     scrollLeft -= (delta * 30);
+    //     event.preventDefault();
+    //     $rollBannerIn.css("left",-scrollLeft+'px')
+    //     var scrollW= this.scrollLeft*(scrollBarMax/scrollMax);
+    //     $scrollBarIn.css("left",scrollW+"px");
+    // });
 
     $scrollBarIn.draggable({
         containment : 'parent', // 부모요소 안에 종속
         drag:function(){
             var pos = $(this).position().left;
-            var leftPos = pos-475;
-            
+            var leftPos = (pos-475)*(scrollMax/scrollBarMax);
+            $rollBannerIn.css("left",-leftPos+'px')
         }
     });
 
